da6459777bd5afad284784aacac9bd7c8a1e05fa
Vineeta Pathak <pathak.vineeta786@gmail.com>
2020-03-24 00:55:13 +0530

    new code added

---
+++ Control Structures/Zero Converter.py
@@ -0,0 +1,21 @@
+def pos(n):
+    ## Write the code
+    if (n==0):
+        print("already Zero")
+    elif(n>0):
+        while(n>0):
+            n=n-1
+            print(n,end =" ")
+            
+    
+def neg(n):
+    ##Write the code
+    if (n==0):
+        print("already Zero")
+    elif(n<0):
+        while(n<=0):
+            
+            print(n,end=" ")
+            n=n+1
+            
+    
