41e6e8afb2004735082967e49c1218393482d66a
Vineeta Pathak <pathak.vineeta786@gmail.com>
2020-05-05 01:19:33 +0530

    updated Code

---
+++ Classes and Functions/Modules.py
@@ -0,0 +1,28 @@
+#{ 
+#Driver Code Starts
+#Initial Template for Python 3
+
+ # } Driver Code Ends
+
+#User function Template for python3
+
+from math import pi ,e
+def math_func():
+    #import math module and print pi and e
+    
+    # Hint : Use from ..... import.....
+    
+    print("pi = "+str(pi))
+   
+    print("e = "+str(e))
+
+#{ 
+#Driver Code Starts.
+
+def main():
+    math_func()
+    
+    
+if __name__ == '__main__':
+    main()
+#} Driver Code Ends
